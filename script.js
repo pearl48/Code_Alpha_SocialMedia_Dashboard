abff849607fc86776693431249e0daf6d427f0b6
Chisom Edeh <160404798+pearl48@users.noreply.github.com>
2024-09-12 10:24:25 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+    // Simulate user login and account connection
+    alert('You are about to be logged in.');
+    fetchFeeds();
+    });
